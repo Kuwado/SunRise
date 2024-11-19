1257833d8bdfea4f19582622a9041d6640d2ebb8
“Kuwado” <viethoan557723@gmail.com>
2024-11-19 18:19:08 +0700

    Dropdown Component

--- resources/js/components/Input/Default/DefaultInput.jsx
+++ resources/js/components/Input/Default/DefaultInput.jsx
@@ -6,10 +6,11 @@ const cx = classNames.bind(styles);
 
 const DefaultInput = ({ value, setValue, label = '', placeholder = '', type = 'text', id, width, required }) => {
     return (
-        <div className={cx('default-input')} style={{ width: width }}>
+        <div className={cx('default-input', { 'no-label': !label })} style={{ width: width }}>
             <label htmlFor={`default-input-${id}`}>
                 {label}
                 {required && <span className={cx('required-note')}>*</span>}
+                {!label && 'no-label'}
             </label>
             <input
                 id={`default-input-${id}`}
