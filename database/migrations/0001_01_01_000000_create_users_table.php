1bb7d5767e42e07241065dcec15708d87b6e47fc
QV142 <cuanne142@gmail.com>
2024-11-21 21:53:30 +0700

    restore database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->unique();
             $table->text('address')->nullable();
@@ -23,8 +24,24 @@ return new class extends Migration
             $table->string('workplace')->nullable();
             $table->string('nationality')->nullable();
             $table->string('city')->nullable();
+            $table->rememberToken();
             $table->timestamps();
             $table->enum('role', ['admin', 'user'])->default('user');
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
@@ -34,5 +51,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
 };
