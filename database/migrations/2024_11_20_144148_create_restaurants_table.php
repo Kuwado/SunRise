46088aa048eed59b4f96399df0b5147370a01c31
QV142 <cuanne142@gmail.com>
2024-11-28 23:59:31 +0700

    fix database

--- database/migrations/2024_11_20_144148_create_restaurants_table.php
+++ database/migrations/2024_11_20_144148_create_restaurants_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('email')->unique();
-            $table->string('phone')->unique();
+            $table->string('phone')->unique()->nullable();
             $table->text('address')->nullable();
             $table->string('avatar')->nullable();
             $table->string('description')->nullable();
