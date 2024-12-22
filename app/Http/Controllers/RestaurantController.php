68df80e3c29d49b9a71d5b2026d97b2b3bb6bbf3
PhamNhatAnh2003 <anhpham090423@gmail.com>
2024-12-23 00:47:14 +0700

    updateVideo

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -44,7 +44,7 @@ class RestaurantController extends Controller
                 'open_time' => 'required|date_format:H:i:s',
                 'close_time' => 'required|date_format:H:i:s',
                 'avatar' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
-                'media.*' => 'nullable',
+                'media.*' => 'nullable|mimes:jpg,jpeg,png,mp4,avi,mkv|max:20480000',
             ];
 
             // Tùy chỉnh thông báo lỗi (nếu cần)
